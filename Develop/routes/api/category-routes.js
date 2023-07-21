1ac454a748f18b36b5f7789c72fcd7d5e6ac92ba
Sam Joseph <josephash1818@gmail.com>
2023-07-21 13:41:05 -0400

    product routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -14,28 +14,90 @@ router.get('/', (req, res) => {
       },
     ]
   })
-  .then((data) => res.json(data))
-  .catch((err) => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    .then((data) => res.json(data))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+      },
+    ]
+  })
+    .then(data => {
+      if (!data) {
+        res.status(404).json({ mesasge: 'Can\'t find a category that matches the ID.' });
+        return;
+      }
+      res.json(data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.json(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name,
+  })
+    .then(data => res.json(data))
+    .catch(err => {
+      console.log(err);
+      res.json(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    }
+  })
+    .then(data => {
+      if (!data[0]) {
+        res.status(404).json({ message: 'ID not found' });
+        return;
+      }
+      res.json(data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then(data => {
+      if (!data) {
+        res.status(404).json({ message: 'ID not found' })
+        return;
+      }
+      res.json(data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
